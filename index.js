52908e0f86c55127222253088fbf68febad4eeeb
Maximilian Osinski <maximilianosinski@maxeman.com>
2022-12-08 16:15:27 +0100

    Added Send & Receive message.

--- index.js
+++ index.js
@@ -23,7 +23,38 @@ wss.on("connection", ws => {
     // Messages
     ws.on("message", data => {
         try {
-            // to-do
+            Logging.logMessage(`${uuid} | Received data.`);
+            const message = JSON.parse(data);
+
+            // Sending Message
+            if(message["header"]["type"] === "SendMessage") {
+                const messageUuid = UUID().v4();
+                const receiver = message["header"]["receiver"];
+                const contentType = message["header"]["contentType"];
+                const content = message["content"];
+                Messages.push(new Message(messageUuid, uuid, receiver, contentType, content));
+                const index = Connections.findIndex(c => c.uuid === receiver);
+                if(index > -1) {
+                    Logging.logMessage(`${uuid} | Transfering message (${messageUuid}) to ${receiver}.`);
+                    Connections[index].ws.send(JSON.stringify({
+                        header: {
+                            type: "ReceiveMessage",
+                        },
+                        message: messageUuid
+                    }));
+                }
+            }
+
+            // Receiving Message
+            if(message["header"]["type"] === "ReceiveMessage") {
+                const messageUuid = message["message"];
+                const index = Messages.findIndex(m => m.uuid === messageUuid);
+                if(index > -1) {
+                    Logging.logMessage(`${uuid} | Received message (${messageUuid}) from ${Messages[index].sender}.`);
+                    delete Messages[index].receiver;
+                    ws.send(JSON.stringify(Messages[index]));
+                }
+            }
         } catch (e) {
             Logging.logMessage(`${uuid} | Error while receiving message: ${e.message}`);
         }
--- message.js
+++ message.js
@@ -2,9 +2,13 @@ module.exports = class Message {
     uuid;
     sender;
     receiver;
-    constructor(uuid, sender, receiver) {
+    contentType;
+    content;
+    constructor(uuid, sender, receiver, contentType, content) {
         this.uuid = uuid;
         this.sender = sender;
         this.receiver = receiver;
+        this.contentType = contentType;
+        this.content = content;
     }
 }
